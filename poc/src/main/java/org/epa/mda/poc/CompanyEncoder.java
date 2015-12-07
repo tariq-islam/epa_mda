e8afceb739406faa8a983aa8178c03851becd6df
tislam <tislam@localhost.localdomain>
2015-12-07 17:34:11 -0500

    removed extra output

--- poc/src/main/java/org/epa/mda/poc/CompanyEncoder.java
+++ poc/src/main/java/org/epa/mda/poc/CompanyEncoder.java
@@ -4,7 +4,6 @@ public class CompanyEncoder {
 	public static String encodeString(String toEncode) {
 		String toReturn = "no%20value";
 		toReturn = toEncode.replaceAll("\\s+", "%20");
-		System.out.println(toReturn);
 		return toReturn;
 	}
 }
