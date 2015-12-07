82cc428cde5b1505e1d7b1882799386941a3cfcd
tislam <tislam@localhost.localdomain>
2015-12-07 14:18:14 -0500

    message

--- poc/src/main/java/org/epa/mda/poc/ParseRESTResponse.java
+++ poc/src/main/java/org/epa/mda/poc/ParseRESTResponse.java
@@ -10,11 +10,11 @@ public class ParseRESTResponse {
 	static KieBase kbase;
 	static KieSession ksession;
 
-	public static void parseResponse(Company company, String restOutput) {
+	public static Company parseResponse(Company company, String restOutput) {
 		System.out.println("Here is what came back from the REST call: " + restOutput);
 		try {
 			ksession = readKnowledgeBase();
-restOutput = "<Results><FRSFacility><RegistryId>110012168359</RegistryId><FacilityName>VFL TECHNOLOGY CORPORATION - PORTABLE 61434</FacilityName><LocationAddress>538 OYSTER POINT ROAD</LocationAddress><SupplementalLocation/><CityName>NEWPORT NEWS</CityName><CountyName>NEWPORT NEWS CITY</CountyName><StateAbbr>VA</StateAbbr><ZipCode>23602-6920</ZipCode><FIPSCode>51700</FIPSCode><Latitude83>37.10338</Latitude83><Longitude83>-76.49904</Longitude83></FRSFacility></Results>";
+//restOutput = "<Results><FRSFacility><RegistryId>110012168359</RegistryId><FacilityName>VFL TECHNOLOGY CORPORATION - PORTABLE 61434</FacilityName><LocationAddress>538 OYSTER POINT ROAD</LocationAddress><SupplementalLocation/><CityName>NEWPORT NEWS</CityName><CountyName>NEWPORT NEWS CITY</CountyName><StateAbbr>VA</StateAbbr><ZipCode>23602-6920</ZipCode><FIPSCode>51700</FIPSCode><Latitude83>37.10338</Latitude83><Longitude83>-76.49904</Longitude83></FRSFacility></Results>";
 			System.out.println(restOutput);
 			String [] splitArray = restOutput.split("</*\\w+/*>");
 			if (splitArray != null) {
@@ -62,6 +62,7 @@ restOutput = "<Results><FRSFacility><RegistryId>110012168359</RegistryId><Facili
 		 * Parse the REST xml string and place data into Company object
 		 * Need to find out if we can update the company object in place or if we need to clear and re-insert.
 		 */
+		return company;
 	}
 
 	private static KieSession readKnowledgeBase() throws Exception {
