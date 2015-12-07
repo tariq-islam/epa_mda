c1173c56918985e96788f21e484630fdf8205af5
tislam <tislam@localhost.localdomain>
2015-12-07 17:04:52 -0500

    Adding the encoder

--- poc/src/main/java/org/epa/mda/poc/CompanyEncoder.java
+++ poc/src/main/java/org/epa/mda/poc/CompanyEncoder.java
@@ -1,17 +1,10 @@
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
+package org.epa.mda.poc;
 
 public class CompanyEncoder {
-	private static String encodingFormat = "UTF-8";
-	private static String toReturn = "no value";
-	
 	public static String encodeString(String toEncode) {
-		try {
-			toReturn = URLEncoder.encode(toEncode, encodingFormat);
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
+		String toReturn = "no%20value";
+		toReturn = toEncode.replaceAll("\\s+", "%20");
+		System.out.println(toReturn);
 		return toReturn;
 	}
 }
